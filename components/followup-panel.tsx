f29e6290a7a69f3e5eb751a8146f2ac2af4d0590
smartyhouses <108938542+smartyhouses@users.noreply.github.com>
2024-08-18 08:07:18 +0300

    Update followup-panel.tsx

--- components/followup-panel.tsx
+++ components/followup-panel.tsx
@@ -47,7 +47,7 @@ export function FollowupPanel() {
       <Input
         type="text"
         name="input"
-        placeholder="Ask a follow-up question..."
+        placeholder="Можем продолжить обсуждать этот вопрос!"
         value={input}
         className="pr-14 h-12"
         onChange={e => setInput(e.target.value)}
